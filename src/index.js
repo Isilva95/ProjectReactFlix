1fb06bdb10b733259550cb9918313a51a956fb38
Isac Santos <isilva_santos@hotmail.com>
2020-08-03 09:46:49 -0300

    index alterado...

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import Home from './pages/Home';
 
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import CadastroVideo from './pages/cadastro/videos';
+import CadastroCategoria from './pages/cadastro/Categoria';
 
 const Pagina404 = () => (<div>Página 404</div>)
 
@@ -13,6 +14,7 @@ ReactDOM.render(
     <Switch>
       <Route path="/" component={Home} exact />
       <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
       <Route component={Pagina404} />
     </Switch>  
   </BrowserRouter>,
